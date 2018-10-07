85a1bc88e6a4f4241a792b7e117d6c7167106ec1
Ketan Sethi <ketansethi786@gmail.com>
2018-10-07 21:20:30 +0530

    spring dao

--- src/main/java/com/amazon/cucumber/TestContext.java
+++ src/main/java/com/amazon/cucumber/TestContext.java
@@ -1,5 +1,9 @@
 package com.amazon.cucumber;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.amazon.managers.FileReaderManager;
 import com.amazon.managers.PageObjectManager;
 import com.amazon.managers.WebDriverManager;
@@ -8,11 +12,16 @@ public class TestContext {
 	private WebDriverManager webDriverManager;
 	private FileReaderManager fileReaderManager;
 	private PageObjectManager pageObjectManager;
+	private JdbcTemplate jdbcTemplateObject;
+	private static final String confFile = "/spring-context/applicationContext-smoke-suite.xml";
+	private static ConfigurableApplicationContext context;
 
 	public TestContext() {
 		webDriverManager = WebDriverManager.getInstance();
 		fileReaderManager = FileReaderManager.getInstance();
 		pageObjectManager = new PageObjectManager(webDriverManager.getDriver());
+		context = new ClassPathXmlApplicationContext(confFile);
+		jdbcTemplateObject = context.getBean(JdbcTemplate.class);
 	}
 
 	public WebDriverManager getWebDriverManager() {
@@ -25,6 +34,10 @@ public class TestContext {
 
 	public FileReaderManager getFileReaderManager() {
 		return fileReaderManager;
+	}
+
+	public JdbcTemplate getTletIdrDao() {
+		return jdbcTemplateObject;
 	}
 
 }
