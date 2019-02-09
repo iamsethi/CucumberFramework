83ff5cd3466e5a98ba7eadf716eb4e72b2561098
ketan.sethi <ketansethi786@gmail.com>
2019-02-09 14:51:21 +0530

    initialize json and register envn

--- src/test/java/com/amazon/api/Test.java
+++ src/test/java/com/amazon/api/Test.java
@@ -1,31 +1,47 @@
 package com.amazon.api;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import com.amazon.managers.FileReaderManager;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class Test {
-	private String dataFile = FileReaderManager.getInstance().getConfigReader().getTestDataResourcePath();
-	Gson gson = new Gson();
+	private static String dataFile = FileReaderManager.getInstance().getConfigReader().getTestDataResourcePath();
+	static Gson gson = new Gson();
+	private static JsonObject jsonObject;
+	private static JsonObject commondata;
 
 	Test() {
-		getEnvironment("ITV1");
+		registerEnvironment("ITV1");
+		initializeJSON();
+
 	}
 
 	public static void main(String args[]) {
 		new Test();
 	}
 
-	public void getEnvironment(String envn) {
-		try {
-			JsonObject jsonObject = gson.fromJson(new FileReader(dataFile), JsonObject.class);
+	public static void registerEnvironment(String envn) {
+		try (FileReader reader = new FileReader(dataFile)) {
+			jsonObject = gson.fromJson(new FileReader(dataFile), JsonObject.class);
 			JsonObject Environments = jsonObject.getAsJsonObject("Environments");
 			System.out.println(Environments.get(envn));
-
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	public static void initializeJSON() {
+		commondata = jsonObject.getAsJsonObject("commondata");
+		System.out.println(commondata);
+
 	}
 }
