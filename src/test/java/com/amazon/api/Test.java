9ae373c0c40ec8b06df503c6a7eedbd72a1b9f20
ketan.sethi <ketansethi786@gmail.com>
2019-02-09 14:35:04 +0530

    common data

--- src/test/java/com/amazon/api/Test.java
+++ src/test/java/com/amazon/api/Test.java
@@ -1,28 +1,28 @@
 package com.amazon.api;
 
 import java.io.FileReader;
-import java.util.Iterator;
 
 import com.amazon.managers.FileReaderManager;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Test {
-	private static String dataFile = FileReaderManager.getInstance().getConfigReader().getTestDataResourcePath();
+	private String dataFile = FileReaderManager.getInstance().getConfigReader().getTestDataResourcePath();
+	Gson gson = new Gson();
+
+	Test() {
+		getEnvironment("ITV1");
+	}
 
 	public static void main(String args[]) {
-		Gson gson = new Gson();
+		new Test();
+	}
+
+	public void getEnvironment(String envn) {
 		try {
 			JsonObject jsonObject = gson.fromJson(new FileReader(dataFile), JsonObject.class);
-			JsonArray Environments = jsonObject.getAsJsonArray("Environments");
-			Iterator<JsonElement> iterator = Environments.iterator();
-			while (iterator.hasNext()) {
-				JsonElement jsonElement = iterator.next();
-				JsonObject itv = jsonElement.getAsJsonObject();
-				System.out.println(itv.keySet());
-			}
+			JsonObject Environments = jsonObject.getAsJsonObject("Environments");
+			System.out.println(Environments.get(envn));
 
 		} catch (Exception e) {
 			e.printStackTrace();
